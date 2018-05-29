b77141b61ba91ab826c3d0487e66b1a2d287afcb
Josh Sample <joshuawsample@gmail.com>
2018-05-29 02:23:44 -0400

    Simple calc commit

---
+++ src/pages/calculator/calculator.ts
@@ -0,0 +1,39 @@
+import { Component } from '@angular/core';
+import { NavController, AlertController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-calculator',
+  templateUrl: 'calculator.html'
+})
+export class CalculatorPage {
+  constructor(public navCtrl: NavController, private alertCtrl: AlertController) {
+  }
+  
+  result = '';
+
+  btnClicked(btn) {
+    if(btn == 'C'){
+      this.result = '';
+    }else if(btn == '='){
+      if(this.result == ''){
+        return;
+      }
+      try {
+        this.result = eval(this.result).toFixed(4);
+      }catch(error) {
+        this.showAlert();
+        this.result = '';
+      }
+      
+    }else{
+      this.result += btn;
+    }
+  }
+  showAlert = () => {
+    this.alertCtrl.create({
+      title: "'Malformed input'",
+      subTitle: 'Ooops, please try again...',
+      buttons: ['Dismiss']
+    }).present();
+  }
+}
